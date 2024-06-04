743782b5bc70b60db6ed58f4401efadc3befe774
xfangfang <2553041586@qq.com>
2024-06-04 15:11:01 +0800

    First available version

--- main.py
+++ main.py
@@ -1,41 +1,210 @@
 import os
-
-# The decky plugin module is located at decky-loader/plugin
-# For easy intellisense checkout the decky-loader code one directory up
-# or add the `decky-loader/plugin` path to `python.analysis.extraPaths` in `.vscode/settings.json`
 import decky_plugin
+from aiohttp import web
+import queue
+import asyncio
 
+def get_plugin_dir():
+    from pathlib import Path
+    return Path(__file__).parent.resolve()
+
+def add_plugin_to_path():
+    import sys
+
+    plugin_dir = get_plugin_dir()
+    decky_plugin.logger.info(f'plugin dir: {plugin_dir}')
+    directories = [["./"], ["dbus_next"], ["lib"], ["py_modules", "dbus_next"], ["py_modules", "lib"]]
+    for dir in directories:
+        sys.path.insert(0, str(plugin_dir.joinpath(*dir)))
+
+def setup_environ_vars():
+    os.environ['XDG_RUNTIME_DIR'] = '/run/user/1000'
+    os.environ['DBUS_SESSION_BUS_ADDRESS'] = 'unix:path=/run/user/1000/bus'
+    os.environ['HOME'] = '/home/deck'
+
+add_plugin_to_path()
+setup_environ_vars()
+
+import x.etree.ElementTree as ET
+
+debug = False
+event_queue = queue.Queue()
+
+async def index(request: web.Request) -> web.StreamResponse:
+    global event_queue
+    query_parameters = request.rel_url.query
+    brightness = query_parameters.get('brightness', None)
+    if brightness is not None:
+        event_queue.put({"type": "brightness", "value": int(brightness)})
+    idle = query_parameters.get('idle', None)
+    idle_type = query_parameters.get('idle_type', None)
+    if idle is not None:
+        event_queue.put({"type": "idle", "value": int(idle), "idle_type": int(idle_type)})
+    return web.Response(text="hello world")
+
+app = web.Application()
+app.router.add_route("GET", "/", index)
+runner = None
+site = None
+
+async def stop_web():
+    global site
+    global runner
+    if site is None:
+        return
+    await site.stop()
+    await runner.cleanup()
+    runner = None
+    site = None
+
+async def start_web():
+    global site
+    global runner
+    await stop_web()
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, '0.0.0.0', 8776)
+    await site.start()
+
+from dbus_next.aio import MessageBus
+from dbus_next import Message, MessageType
+from dbus_next.service import (ServiceInterface, method, dbus_property, signal)
+
+ignore_application = ["Steam", "./steamwebhelper"]
+bus = None
+
+class InhibitInterface(ServiceInterface):
+    def __init__(self):
+        super().__init__('org.freedesktop.ScreenSaver')
+
+    @method()
+    def Inhibit(self, application: 's', reason: 's') -> 'u':
+        if application in ignore_application: return 2
+        decky_plugin.logger.info(f'called Inhibit with application={application} and reason={reason}')
+        event_queue.put({"type": "Inhibit"})
+        return 1
+
+    @method()
+    def UnInhibit(self, cookie: 'u'):
+        if cookie != 1: return
+        decky_plugin.logger.info(f'called UnInhibit with cookie={cookie}')
+        event_queue.put({"type": "UnInhibit"})
+
+class PMInhibitInterface(ServiceInterface):
+    def __init__(self):
+        super().__init__('org.freedesktop.PowerManagement.Inhibit')
+
+    @method()
+    async def Inhibit(self, application: 's', reason: 's') -> 'u':
+        if application in ignore_application: return 2
+        decky_plugin.logger.info(f'called Inhibit with application={application} and reason={reason}')
+        event_queue.put({"type": "Inhibit"})
+
+        # sender = self.last_msg.sender
+        # decky_plugin.logger.info(f'Sender: {sender}')
+        # reply = await bus.call(Message(
+        #     destination='org.freedesktop.DBus',
+        #     path='/org/freedesktop/DBus',
+        #     interface='org.freedesktop.DBus',
+        #     member='GetConnectionUnixProcessID',
+        #     signature='s',
+        #     body=[sender]
+        # ))
+        # if reply.message_type == MessageType.ERROR:
+        #     decky_plugin.logger.info(f'error: {reply.body[0]}')
+        # else:
+        #     decky_plugin.logger.info(f'Process ID: {reply.body[0]} - {type(reply.body[0])}')
+
+        return 1
+
+    @method()
+    def UnInhibit(self, cookie: 'u'):
+        if cookie != 1: return
+        decky_plugin.logger.info(f'called UnInhibit with cookie={cookie}')
+        event_queue.put({"type": "UnInhibit"})
+
+class PortalInhibitInterface(ServiceInterface):
+    def __init__(self):
+        super().__init__('org.freedesktop.portal.Inhibit')
+
+    @method()
+    def Inhibit(self, application: 's', reason: 's') -> 'u':
+        if application in ignore_application: return 2
+        print(f'called Inhibit with application={application} and reason={reason}')
+        event_queue.put({"type": "Inhibit"})
+        return 1
+
+    @method()
+    def UnInhibit(self, cookie: 'u'):
+        if cookie != 1: return
+        print(f'called UnInhibit with cookie={cookie}')
+        event_queue.put({"type": "UnInhibit"})
+
+async def stop_dbus():
+    global bus
+    try:
+        if bus is not None:
+            bus.disconnect()
+        bus = None
+    except Exception as e:
+        decky_plugin.logger.info(f"error: {e}")
+
+async def start_dbus():
+    global bus
+    await stop_dbus()
+    try:
+        bus = await MessageBus().connect()
+        interface = InhibitInterface()
+        pm_interface = PMInhibitInterface()
+        portal_interface = PortalInhibitInterface()
+        bus.export('/ScreenSaver', interface) # vlc
+        bus.export('/org/freedesktop/ScreenSaver', interface) # chrome
+        bus.export('/org/freedesktop/PowerManagement/Inhibit', pm_interface) # wiliwili
+        bus.export('/org/freedesktop/portal/desktop', portal_interface) # firefox
+        await bus.request_name('org.freedesktop.PowerManagement')
+        await bus.request_name('org.freedesktop.ScreenSaver')
+        await bus.request_name('org.freedesktop.portal.Desktop')
+    except Exception as e:
+        decky_plugin.logger.info(f"error: {e}")
 
 class Plugin:
-    # A normal method. It can be called from JavaScript using call_plugin_function("method_1", argument1, argument2)
-    async def add(self, left, right):
-        return left + right
 
-    # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
+    async def start_backend(self):
+        decky_plugin.logger.info("Start backend server")
+        if debug:
+            await start_web()
+        else:
+            await start_dbus()
+
+    async def stop_backend(self):
+        decky_plugin.logger.info("Stop backend server")
+        if debug:
+            await stop_web()
+        else:
+            await stop_dbus()
+        event_queue.queue.clear()
+
+    async def get_event(self):
+        res = []
+        while not event_queue.empty():
+            try:
+                res.append(event_queue.get_nowait())
+            except queue.Empty:
+                continue
+        return res
+
     async def _main(self):
         decky_plugin.logger.info("Hello World!")
 
-    # Function called first during the unload process, utilize this to handle your plugin being removed
     async def _unload(self):
-        decky_plugin.logger.info("Goodbye World!")
+        decky_plugin.logger.info("Goodnight World!")
+        if debug:
+            stop_web()
+        else:
+            stop_dbus()
+
+    async def _uninstall(self):
         pass
 
-    # Migrations that should be performed before entering `_main()`.
     async def _migration(self):
-        decky_plugin.logger.info("Migrating")
-        # Here's a migration example for logs:
-        # - `~/.config/decky-template/template.log` will be migrated to `decky_plugin.DECKY_PLUGIN_LOG_DIR/template.log`
-        decky_plugin.migrate_logs(os.path.join(decky_plugin.DECKY_USER_HOME,
-                                               ".config", "decky-template", "template.log"))
-        # Here's a migration example for settings:
-        # - `~/homebrew/settings/template.json` is migrated to `decky_plugin.DECKY_PLUGIN_SETTINGS_DIR/template.json`
-        # - `~/.config/decky-template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_SETTINGS_DIR/`
-        decky_plugin.migrate_settings(
-            os.path.join(decky_plugin.DECKY_HOME, "settings", "template.json"),
-            os.path.join(decky_plugin.DECKY_USER_HOME, ".config", "decky-template"))
-        # Here's a migration example for runtime data:
-        # - `~/homebrew/template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_RUNTIME_DIR/`
-        # - `~/.local/share/decky-template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_RUNTIME_DIR/`
-        decky_plugin.migrate_runtime(
-            os.path.join(decky_plugin.DECKY_HOME, "template"),
-            os.path.join(decky_plugin.DECKY_USER_HOME, ".local", "share", "decky-template"))
+        pass
--- src/index.tsx
+++ src/index.tsx
@@ -1,105 +1,139 @@
 import {
-  ButtonItem,
   definePlugin,
-  DialogButton,
-  Menu,
-  MenuItem,
-  Navigation,
+  ToggleField,
   PanelSection,
   PanelSectionRow,
   ServerAPI,
-  showContextMenu,
   staticClasses,
 } from "decky-frontend-lib";
 import { VFC } from "react";
-import { FaShip } from "react-icons/fa";
+import { useState } from 'react'
+import { GiNightSleep } from "react-icons/gi";
 
-import logo from "../assets/logo.png";
+let backendRunning = true;
 
-// interface AddMethodArgs {
-//   left: number;
-//   right: number;
-// }
-
-const Content: VFC<{ serverAPI: ServerAPI }> = ({serverAPI}) => {
-  // const [result, setResult] = useState<number | undefined>();
-
-  // const onClick = async () => {
-  //   const result = await serverAPI.callPluginMethod<AddMethodArgs, number>(
-  //     "add",
-  //     {
-  //       left: 2,
-  //       right: 2,
-  //     }
-  //   );
-  //   if (result.success) {
-  //     setResult(result.result);
-  //   }
-  // };
-
+const Content: VFC<{ serverApi: ServerAPI }> = ({serverApi}) => {
+  const [running, setRunning] = useState<boolean>(backendRunning);
   return (
-    <PanelSection title="Panel Section">
+    <PanelSection title="Settings">
       <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={(e) =>
-            showContextMenu(
-              <Menu label="Menu" cancelText="CAAAANCEL" onCancel={() => {}}>
-                <MenuItem onSelected={() => {}}>Item #1</MenuItem>
-                <MenuItem onSelected={() => {}}>Item #2</MenuItem>
-                <MenuItem onSelected={() => {}}>Item #3</MenuItem>
-              </Menu>,
-              e.currentTarget ?? window
-            )
-          }
-        >
-          Server says yolo
-        </ButtonItem>
-      </PanelSectionRow>
-
-      <PanelSectionRow>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <img src={logo} />
-        </div>
-      </PanelSectionRow>
-
-      <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={() => {
-            Navigation.CloseSideMenus();
-            Navigation.Navigate("/decky-plugin-test");
+        <ToggleField
+          label='Start'
+          onChange={async (checked) => {
+            setRunning(checked)
+            DeckyPluginLoader.toaster.toast({
+              title: "视频播放检测",
+              body: checked ? "已开启": "已关闭",
+              icon: <GiNightSleep />,
+            });
+            await serverApi.callPluginMethod<any, any>(checked ? "start_backend" : "stop_backend", {});
           }}
-        >
-          Router
-        </ButtonItem>
+          checked={running}
+          />
       </PanelSectionRow>
     </PanelSection>
   );
 };
 
-const DeckyPluginRouterTest: VFC = () => {
-  return (
-    <div style={{ marginTop: "50px", color: "white" }}>
-      Hello World!
-      <DialogButton onClick={() => Navigation.NavigateToLibraryTab()}>
-        Go to Library
-      </DialogButton>
-    </div>
-  );
-};
-
 export default definePlugin((serverApi: ServerAPI) => {
-  serverApi.routerHook.addRoute("/decky-plugin-test", DeckyPluginRouterTest, {
-    exact: true,
-  });
+  console.debug("on Start");
+  let requestChanging = 0;
+  function onSettingsChanges(buffer: ArrayBuffer) {
+    let view        = new DataView(buffer);
+    let ac_idle         = view.getFloat32(6, true);
+    let battery_idle    = view.getFloat32(1, true);
+    let ac_suspend      = view.getFloat32(16, true);
+    let battery_suspend = view.getFloat32(11,true)
+    console.debug(`${ac_idle}, ${battery_idle}, ${ac_suspend}, ${battery_suspend}`)
+    if (requestChanging <= 0 && backendRunning) {
+      // 用户手动修改
+      // 1. 停止后端运行
+      backendRunning = false;
+      setTimeout(async () => {
+        await serverApi.callPluginMethod<any, any>("stop_backend", {});
+      }, 0);
+
+      // 2. 弹出提示
+      DeckyPluginLoader.toaster.toast({
+        title: "视频播放检测",
+        body: "已关闭",
+        icon: <GiNightSleep />,
+      });
+      requestChanging = 0;
+    } else {
+      requestChanging--;
+    }
+    
+  };
+
+  // 注册回调函数时会触发一次 onSettingsChanges
+  requestChanging = 1;
+  let handle = SteamClient.System.RegisterForSettingsChanges(onSettingsChanges);
+
+  // index:
+  // 1: battery_idle
+  // 2: ac_idle
+  // 3: battery_suspend
+  // 4: ac_suspend
+  //
+  // data:
+  // 0 for disable (seconds)
+  function genSettings(index: number, data: number) {
+    let buffer = new ArrayBuffer(5);
+    let view = new DataView(buffer);
+    view.setUint8(0, index << 3 | 5);
+    view.setFloat32(1, data, true);
+    let binary = '';
+    let bytes = new Uint8Array(buffer);
+    let len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }
+    return binary;
+  }
+
+  const getEvent = async () => {
+    return await serverApi.callPluginMethod<any, any>("get_event", {});
+  }
+
+  const updateSetting = async (data: string) => {
+    await SteamClient.System.UpdateSettings(window.btoa(data))
+  }
+
+  let interval = setInterval(async () => {
+    let data = await getEvent();
+    if(!data.result) return;
+    console.debug(data)
+    let event = data.result;
+    for (let e of event) {
+      if (e.type == 'Inhibit') {
+        requestChanging++;
+        await updateSetting(genSettings(1, 0)+genSettings(2, 0)+genSettings(3, 0)+genSettings(4, 0));
+      } else if (e.type == 'UnInhibit') {
+        // todo: restore
+        requestChanging++;
+        await updateSetting(genSettings(1, 600)+genSettings(2, 600)+genSettings(3, 600)+genSettings(4, 600));
+      } if (e.type == 'brightness') {
+        await SteamClient.System.Display.SetBrightness(e.value / 100);
+      } else if (e.type == 'idle') {
+        requestChanging++;
+        await updateSetting(genSettings(e.idle_type, e.value));
+      }
+    }
+  }, 1000)
+
+  setTimeout(async () => {
+    await serverApi.callPluginMethod<any, any>("start_backend", {});
+  }, 0);
 
   return {
-    title: <div className={staticClasses.Title}>Example Plugin</div>,
-    content: <Content serverAPI={serverApi} />,
-    icon: <FaShip />,
+    title: <div className={staticClasses.Title}>Suspend Manager</div>,
+    content: <Content serverApi={serverApi} />,
+    icon: <GiNightSleep />,
     onDismount() {
-      serverApi.routerHook.removeRoute("/decky-plugin-test");
+      console.debug("on Dismount");
+      clearInterval(interval);
+      if (handle) handle.unregister();
     },
   };
 });
